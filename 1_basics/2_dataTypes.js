422bc626bdc1c6c0307ed3704925677355792150
Mansi Kathale <89722492+itsmansikathale@users.noreply.github.com>
2024-05-10 12:28:28 +0000

    DataTypes

---
+++ 1_basics/2_dataTypes.js
@@ -0,0 +1,36 @@
+"use strict"; 
+// treat all JS code as a latest version
+// alert(3+3)=>gives an error as we are using node.js,not browser 
+
+console.log(3
+    +
+    3)
+    // code readability should be high
+
+
+// data types in js
+// Primitive datatypes
+
+let address = null;
+let name= "Mansi"
+let age= 16;
+let isLoggedin = false;
+
+// 1 number => 2 to power 53
+// 2 bigint => Specifies more than in nos.
+// 3 string => ""
+// 4 boolean => true/ false
+// 5 null => Empty (Standalone value)
+// 6 undefined => 
+// 7 symbol > Unique symbol
+
+// 8 Object 
+
+console.log(typeof null);  
+// object 
+
+console.log(typeof undefined);
+// undefineds
+
+
+
