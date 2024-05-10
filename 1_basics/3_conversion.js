41a75456d8f6a72b4961272ff071bf323e3f0488
Mansi Kathale <89722492+itsmansikathale@users.noreply.github.com>
2024-05-10 17:23:16 +0000

    Conversion

---
+++ 1_basics/3_conversion.js
@@ -0,0 +1,26 @@
+let score = "mansi"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" = 33
+// "12afk" = NaN (Not a Number)
+// 1= true ; 0 = false
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 = true ; 0 = false 
+// "" = false
+// "Mansi" = true
+
+let SomeNum = 5
+let stringNumber = String(SomeNum)
+console.log(typeof SomeNum);
+console.log(typeof stringNumber);
