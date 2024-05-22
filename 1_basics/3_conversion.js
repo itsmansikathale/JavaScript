a1ac5eded59243641095830d8a3a1e254c2aa94f
Mansi Kathale <89722492+itsmansikathale@users.noreply.github.com>
2024-05-22 11:17:47 +0000

    Operation_conversion

--- 1_basics/3_conversion.js
+++ 1_basics/3_conversion.js
@@ -1,11 +1,11 @@
 let score = "mansi"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number (score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" = 33
 // "12afk" = NaN (Not a Number)
@@ -14,7 +14,7 @@ console.log(valueInNumber);
 let isLoggedIn = ""
 
 let booleanIsLoggedIn = Boolean (isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 = true ; 0 = false 
 // "" = false
@@ -22,5 +22,43 @@ console.log(booleanIsLoggedIn);
 
 let SomeNum = 5
 let stringNumber = String(SomeNum)
-console.log(typeof SomeNum);
-console.log(typeof stringNumber);
+// console.log(typeof SomeNum);
+// console.log(typeof stringNumber);
+
+
+// ******************* Operations *******************
+
+let value = 3
+let negValue = - value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%4);
+
+let str1 = "hello"
+let str2 = " mansi"
+
+let str3 = str1 + str2
+// console.log(str3); 
+
+// console.log("1" +1 )
+// console.log(2+ "1")
+// console.log("1" + "1")
+// console.log(1+2+"3")
+
+// console.log(true);
+// console.log(+true);
+
+// console.log(+"")
+
+let num1, num2, num3
+num1=num2=num3 = 3+2
+
+let gameCounter = 100
+++gameCounter ;
+gameCounter-- ;
+console.log(gameCounter);
