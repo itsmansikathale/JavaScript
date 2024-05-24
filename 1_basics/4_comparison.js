3850963dca769983754f04664a394557d7705f09
Mansi Kathale <89722492+itsmansikathale@users.noreply.github.com>
2024-05-24 10:39:05 +0000

    Comparison

---
+++ 1_basics/4_comparison.js
@@ -0,0 +1,29 @@
+//   To compare all these numbers it is important that 
+//  the datatype should be equal
+
+//  If "2" < 1 in this case it will string 2 to integer and 
+// returns the output
+
+// If there is === in this case it will give an error and will not 
+// convert string to some other data types
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 >= 1);
+console.log(2 == 1);
+console.log(2 <= 1);
+console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===triple check 
+
