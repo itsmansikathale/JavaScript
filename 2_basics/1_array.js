0f1c8d12f438b7142e5780e3518037b0bf85f026
Mansi Kathale <89722492+itsmansikathale@users.noreply.github.com>
2024-07-27 13:28:19 +0000

    Array intro

---
+++ 2_basics/1_array.js
@@ -0,0 +1,15 @@
+// Array is storing a collection of multiple 
+// items under a single variable name
+
+// JS array are resizable
+
+// mixing of different datatypes also possible 
+
+// they are not associative arrays
+// indexing starts from 0
+// Js works on by making shallow copy(call by reference)
+
+const arr = [0,3,2 ,8, "mansi" ]
+
+
+console.log(arr[0])
