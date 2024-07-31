ff032458efad8d1ac1427601ae9bd8b13420e2a9
Mansi Kathale <89722492+itsmansikathale@users.noreply.github.com>
2024-07-31 13:41:03 +0000

    Objects Part 2

--- 2_basics/2_objects.js
+++ 2_basics/2_objects.js
@@ -13,12 +13,12 @@
 // how to add symbol into keys 
 // we use [] to work as a symbol
 const sym = Symbol("hi")
-const user= {
+const user = {
     name: "mansi",
-     "full name": "Mansi Kathale",
-     [sym]:"hi",
+    "full name": "Mansi Kathale",
+    [sym]: "hi",
     "email": "mansi@gmail.com",
-    isLoggedIn : false,
+    isLoggedIn: false,
     lastLoginDays: ["Monday", "Tuesday"]
 }
 
@@ -30,7 +30,7 @@ const user= {
 // console.log(user["full name"])
 // console.log(user[sym]);
 
-user.email= "mansikathale@gmail.com"
+user.email = "mansikathale@gmail.com"
 
 // to lock particular info
 // Object.freeze(user)
@@ -38,13 +38,105 @@ user.email= "mansikathale@gmail.com"
 // console.log(user);
 
 // funtions adding in objects
-user.greeting = function(){
+user.greeting = function () {
     console.log("hello User");
 }
-user.greetingTwo = function(){
+user.greetingTwo = function () {
     // `` this is string interpulation
-    console.log(`hello User, ${this.name}`);
+    // console.log(`hello User, ${this.name}`);
 }
 
-console.log(user.greeting());
-console.log(user.greetingTwo());
+// console.log(user.greeting());
+// console.log(user.greetingTwo());
+
+//------------- Objects(Singleton & Constructor) ------------//
+// () its a parenthesis
+
+// it is a singleton object 
+// const user = new Object ()
+
+// it is a non singleton object
+const hiUser = {}
+
+user.id = "123"
+user.name = "abc"
+user.isLoggedIn = false
+
+// console.log(user);
+
+
+//  Objects under Objects
+const user1 = {
+    email: "abc@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "mansi",
+            lastname: "kathale"
+        }
+
+    }
+}
+
+// console.log(user1.fullname.userfullname);
+
+
+const obj1 ={1:"a" , 2:"b"}
+const obj2 ={3:"a" , 4:"b"}
+const obj4 ={5:"a" , 6:"b"}
+
+// combining two objects
+
+// const obj3 =Object.assign({}, obj1, obj2, obj4)
+// creating an empty object so that the value of all objects
+//  are not stored in the first object i.e obj1
+
+const obj3 ={...obj1, ...obj2}
+// * 90% time we make use this syntax where we are using 
+// spread Operator
+// console.log(obj3);
+
+
+// - when values come from databases
+
+const users = [
+    {
+        id: 1, 
+        email: "a@gmail.com"
+    },
+    {
+        id: 1, 
+        email: "a@gmail.com"
+    },
+    
+    {
+        id: 1, 
+        email: "a@gmail.com"
+    },
+    
+    {
+        id: 1, 
+        email: "a@gmail.com"
+    },
+    
+    {
+        id: 1, 
+        email: "a@gmail.com"
+    },
+    
+    {
+        id: 1, 
+        email: "a@gmail.com"
+    },
+]   
+ users[1].email
+ console.log(hiUser);
+
+ console.log(Object.keys(hiUser));
+ console.log(Object.values(hiUser));
+ console.log(Object.entries(hiUser));
+//  it results array inside array
+
+
+// to check if the object has this property or not
+// when value crashed
+console.log(hiUser.hasOwnProperty('isLoggedIn'));
