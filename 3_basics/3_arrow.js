13a746b2b4e16a95ed1ee5c12143d04f8be1676a
Mansi Kathale <89722492+itsmansikathale@users.noreply.github.com>
2024-09-25 13:15:44 +0000

    Arrow function in depth

--- 3_basics/3_arrow.js
+++ 3_basics/3_arrow.js
@@ -1,4 +1,4 @@
-// this keyword is used for the current context 
+// this keyword is used for refering the current context 
 // this keyword talks about the current value 
 
 const user = {
@@ -36,8 +36,61 @@ console.log(this)
 // browser main
 
 
-function one() {
-    console.log(this);
-    
-}
-one()
+// function one() {
+//     let username = "mansi"
+//     console.log(this); 
+// o/p : undefined
+//     // it will not print the actual o/p inside function 
+//     // as this will not print objects inside functions
+// }
+// one()
+
+
+// const two  = function() {
+//     let username = "mansi"
+//     console.log(this.username);
+// }
+// two()
+// returns undefined again
+
+
+// Ways to declare functions especially arrow function
+
+// if we remove function from the above stmt and add
+//  => 
+// this will become arrow function
+// const two  = () => {
+//     let username = "mansi"
+//     console.log(this.username);
+    // if we completely print this the o/p is {}
+    // if we remove username o/p is undefined
+    // this can be used in arrow function as well
+// }
+
+// two()
+
+// types / ways to declare arrow function
+//1) basic arrow function 
+// or explicit return
+// const addTwo= (num1, num2) => {
+//  return num1 + num2   
+// }
+// console.log(addTwo(10,20))
+
+// when we make use of parenthesis {} then we have
+// to make use of return keyword 
+
+// 2) Implicit arrow function : 
+// Implicit matlab Maan liya 
+// here we remove the return keyword 
+//  and and add them in same line
+
+// const addTwo = (num1, num2) => num1 + num2
+// or 
+// const addTwo = (num1, num2) => (num1 + num2)
+
+// running this with objects
+ const addTwo = (num1, num2) => ({username:'mansi'})
+console.log(addTwo(10,20))
+//In both the above cases we dont need to use return keyword
+
