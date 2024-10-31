ccb206011dba156ee017069a6c1e230873cc4df1
Mansi Kathale <89722492+itsmansikathale@users.noreply.github.com>
2024-10-31 14:46:36 +0000

    If Statement

---
+++ 4_control_flow/one.js
@@ -0,0 +1,40 @@
+// Control flow / logic flow 
+//  We are making use of these stmt because we dont want that 
+// all the unnecessary code should also work.
+
+// Note : single = is an assignment Operator
+
+
+// Control Statement
+// 1 ) if
+
+if (condition) {
+    
+}
+
+//  Yaha par jo if stmt hai wo true hona chahiye
+if (condition= true ){
+    // Agar aisa hua to hi Statement chalenge 
+}
+
+
+
+if (condition = false ) {
+     // if its not true then the code will not execute 
+}
+// Example
+
+const isUserLoggedIn = true
+
+if (isUserLoggedIn) {
+    // iske andar jo koi bhi stmt hoga vo execute hoga
+}
+// There are some  comparison Operators
+{/* < less than
+> Greater than 
+<= Less than equal to
+>= Greater than equal to
+== Equal to
+! NOt Equal to {/* if (3 != 2) */}
+// === This will check type 
+
