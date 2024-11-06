2e53a6aa52efc2ba6f5738ecba14a99a59a705d5
Mansi Kathale <89722492+itsmansikathale@users.noreply.github.com>
2024-11-06 15:00:40 +0000

    Block level scope

--- 4_control_flow/one.js
+++ 4_control_flow/one.js
@@ -8,27 +8,27 @@
 // Control Statement
 // 1 ) if
 
-if (condition) {
+// if (condition) {
     
-}
+// }
 
 //  Yaha par jo if stmt hai wo true hona chahiye
-if (condition= true ){
-    // Agar aisa hua to hi Statement chalenge 
-}
+// if (condition= true ){
+//     // Agar aisa hua to hi Statement chalenge 
+// }
 
 
 
-if (condition = false ) {
-     // if its not true then the code will not execute 
-}
+// if (condition = false ) {
+//      // if its not true then the code will not execute 
+// }
 // Example
 
-const isUserLoggedIn = true
+// const isUserLoggedIn = true
 
-if (isUserLoggedIn) {
-    // iske andar jo koi bhi stmt hoga vo execute hoga
-}
+// if (isUserLoggedIn) {
+//     // iske andar jo koi bhi stmt hoga vo execute hoga
+// }
 // There are some  comparison Operators
 {/* < less than
 > Greater than 
@@ -38,3 +38,36 @@ if (isUserLoggedIn) {
 ! NOt Equal to {/* if (3 != 2) */}
 // === This will check type 
 
+
+const  temp = 10
+if(temp === 10) {
+    console.log("less than 50");
+    
+} else{
+    console.log("greater than 50");
+    
+}
+
+
+// Concept related to Scope
+
+const score = 200
+
+if (score > 100) {
+    const power = "fly"
+//  yaha pe agar const ke jagah pe var hoga to problem dega.
+//  is liye yaha pe sirf const ya let ka hi use karein.
+// var ye global variable hai is liye ye out of scope bhi run karega .
+//  aur ye ek best practice nahi hai for coding
+    console.log(`User power : ${power}`);
+
+    
+    // yaha tak to sara code thik se chalega 
+        //  problem tab start hogi jab hum iss code niche 
+// diye gaye scoope ke bahar print karne ki koshish karenge
+}
+
+//  har bar jab bhi {} ye lagta hain to ek alag scope 
+// count hota hain.
+console.log(`User power : ${power}`);
+//  error dikhayenga ke power is not defined 
