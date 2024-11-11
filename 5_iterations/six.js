d0aca93853be6734ea5905a80990294d0f9dbbf5
Mansi Kathale <89722492+itsmansikathale@users.noreply.github.com>
2024-11-11 12:23:51 +0000

    filter

---
+++ 5_iterations/six.js
@@ -0,0 +1,24 @@
+// const coding = ["js", "cpp", "java"]
+
+// const values =  coding.forEach( (item)=>{
+//     // console.log(item);
+//     return item
+// } )
+
+// console.log(values);
+
+
+// returns undefined as output
+
+// Filters 
+
+const myNums = [1,2,3,4,5,6,7,8,9]
+
+const newNums = myNums.filter ( (num) =>{
+    return num > 5
+})
+console.log(newNums);
+
+// Notes: 
+// it is important to add return ,
+// after that u can the condition
